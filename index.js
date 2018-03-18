05cda5a8d7eeb882cbdfd66781ee3a9ac9d0ce4b
Grant Sheridan <she08001@byui.edu>
2018-03-17 18:55:12 -0600

    added an endpoint

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -8,7 +16,15 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/user/:username/:password', (req, res) => res.render('pages/index'))
-  .get('/books', (req, res) => res.render('pages/index'))
+  .get('/books', function(req, res) {
+    client.query('SELECT * FROM book;', (err, res) => {
+      if (err) throw err;
+      for (let row of res.rows) {
+        console.log(JSON.stringify(row));
+      }
+      client.end();
+    });
+  })
   .get('/author', (req, res) => res.render('pages/index'))
   .get('/comments', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
