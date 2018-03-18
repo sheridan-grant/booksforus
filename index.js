6cecc0b88201625baf4c78fa8f26406ac0f99d3f
Grant Sheridan <she08001@byui.edu>
2018-03-17 19:14:49 -0600

    refining first endpoint

--- index.js
+++ index.js
@@ -16,21 +16,21 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/books', function(req, res) {
-    client.query('SELECT * FROM book;', (err, res) => {
-      if (err) throw err;
-      var rows = [];
-      for (let row of res.rows) {
-        rows.push(JSON.stringify(row));
-      }
-      client.end();
+    // client.query('SELECT * FROM book;', (err, res) => {
+    //   if (err) throw err;
+    //   var rows = [];
+    //   for (let row of res.rows) {
+    //     rows.push(JSON.stringify(row));
+    //   }
+    //   client.end();
+    // });
 
-      res.writeHead(200, {
-        'Content-Type': 'text/html'
-      });
-      res.write('<!doctype html>\n<html lang="en">\n' +
-      '\n<meta charset="utf-8">\n<title>Books</title>\n' +
-      '\n\n<h1>Here are the books</h1>\n' + rows.join(' :: ') + '\n\n');
-      res.end();
+    res.writeHead(200, {
+      'Content-Type': 'text/html'
     });
+    res.write('<!doctype html>\n<html lang="en">\n' +
+    '\n<meta charset="utf-8">\n<title>Books</title>\n' +
+    '\n\n<h1>Here are the books</h1>\n');
+    res.end();
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
