05b67ec0ebe37dd03ee7b659053699dc33ec8ddb
Grant Sheridan <she08001@byui.edu>
2018-03-19 19:44:09 -0600

    refining endpoint

--- index.js
+++ index.js
@@ -3,11 +3,6 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 const { Client } = require('pg');
 
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true,
-});
-
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
@@ -31,6 +26,11 @@ function bookRoute(req, res) {
 }
 
 function getBooks(callback) {
+  var client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+  });
+  
   client.connect(function(err) {
     if (err) {
       console.log("Error connecting to DB: ")
