d425e747a00ce29d715ab8f761e81ed55945bfd8
sheridan-grant <she08001@byui.edu>
2018-03-31 09:04:17 -0600

    filter by title and score

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -2,6 +2,10 @@ angular.module('booksForUs', [])
 
 .controller('bookViewer', [ '$scope', '$http', function($scope, $http) {
   $scope.currentBooks = [];
+  $scope.titleAcsend = false;
+  $scope.titleDesend = false;
+  $scope.scoreAcsend = false;
+  $scope.scoreDesend = false;
 
   $http.get("/books")
     .then(function(books) {
@@ -16,4 +20,42 @@ angular.module('booksForUs', [])
         });
       }
     });
+
+  $scope.titleFilter = function(filterType) {
+    if (filterType == 1) {
+      $scope.titleAcsend = true;
+      $scope.titleDesend = false;
+    } else {
+      $scope.titleAcsend = false;
+      $scope.titleDesend = true;
+    }
+  }
+
+  $scope.scoreFilter = function(filterType) {
+    if (filterType == 1) {
+      $scope.scoreAcsend = true;
+      $scope.scoreDesend = false;
+    } else {
+      $scope.scoreAcsend = false;
+      $scope.scoreDesend = true;
+    }
+  }
+
+  $scope.filterBooksBy = function() {
+    var sortingParams = [];
+
+    if ($scope.titleAcsend) {
+      sortingParams.push('title');
+    } else if ($scope.titleDesend) {
+      sortingParams.push('-title');
+    }
+
+    if ($scope.scoreAcsend) {
+      sortingParams.push('title');
+    } else if ($scope.scoreDesend) {
+      sortingParams.push('-title');
+    }
+
+    return sortingParams;
+  }
 }]);
